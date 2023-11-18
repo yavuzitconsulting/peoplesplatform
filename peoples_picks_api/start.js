b5dc9e31841a1df9682a32cd6796056c4d47ba6a
Sercan Ali Yavuz <sercan.yavuz@adnova.gmbh>
2023-11-18 21:07:08 +0100

    WIP

--- peoples_picks_api/start.js
+++ peoples_picks_api/start.js
@@ -138,7 +138,7 @@ const contractABI = [
 ];
 
 const chainData = {
-  cryptng: {
+  "501984": {
     rpcurl: "https://testnet.cryptng.xyz:8545",
     contractAddress: "0x188C8d37fb966713CbDc7cCc1A6ed3da060FFac3",
     data: [],
@@ -146,50 +146,49 @@ const chainData = {
     fromBlock: 0,
     toBlock: 0,
     lastBlockNumber: undefined,
+    
   },
-  chiado: {
+  "10200": {
     rpcurl: "https://rpc.chiadochain.net",
     contractAddress: "0xffC39C76C68834FE1149554Ccc1a76C2F1281beD",
     data: [],
     latestBlockNumber: 0,
-    fromBlock: 0,
+    fromBlock: 7032222,
     toBlock: 0,
     lastBlockNumber: undefined,
   },
-  sepolia: {
+  "534351": {
     rpcurl: "https://sepolia-rpc.scroll.io/",
     contractAddress: "0x314AA36352771307E942FaeD6d8dfB2398916E92",
     data: [],
     latestBlockNumber: 0,
-    fromBlock: 0,
+    fromBlock: 2305820,
     toBlock: 0,
     lastBlockNumber: undefined,
   },
-  neonlabs: {
+  "245022926": {
     rpcurl: "https://devnet.neonevm.org",
     contractAddress: "0x9A1554a110A593b5C137643529FAA258a710245C",
     data: [],
     latestBlockNumber: 0,
-    fromBlock: 0,
+    fromBlock: 259098931,
     toBlock: 0,
     lastBlockNumber: undefined,
   }
 };
-
-let activeChain = chainData.cryptng; // Set the initial active chain
-
-let _provider = new ethers.JsonRpcProvider(activeChain.rpcurl);
-let _directNetworkContract = new ethers.Contract(activeChain.contractAddress, contractABI, _provider);
-const _interface = _directNetworkContract.interface;
-
+//loop through chainData object keys and add provider, contract and interfaces
+Object.keys(chainData).forEach(key => {
+  let chain = chainData[key];
+  chain.provider = new ethers.JsonRpcProvider(chain.rpcurl);
+  chain.contract = new ethers.Contract(chain.contractAddress, contractABI, chain.provider);
+  chain.interface = chain.contract.interface;
+})
 
 app.use(cors());
 
 let data = [];
 let refreshInterval = 5;
-
-let intervalId = setInterval(loadData, refreshInterval * 1000); 
-
+let intervalId = null;
 const itemsPerPage = 30;
 
 app.get('/aggregate', async (req, res) => {
@@ -207,36 +206,31 @@ app.get('/aggregate', async (req, res) => {
 
       if (selectedChain) {
         // Change the active chain
-        activeChain = selectedChain;
-        console.log(`current chain ${activeChain} with length: ${activeChain.data.length}`);
-        _provider = new ethers.JsonRpcProvider(activeChain.rpcurl);
-        _directNetworkContract = new ethers.Contract(activeChain.contractAddress, contractABI, _provider);
-  
+
         // Check if the chain data is already loaded
-        if (activeChain.data.length === 0) {
-          await loadData();
+          await loadData(selectedChain);
           console.log(`Active chain changed to ${chainIdentifier}`);
-        }
+        
   
       } else {
         return res.status(404).send('Chain identifier not found.');
       }
-    }
+    
 
     const startIndex = (page - 1) * itemsPerPage;
     const endIndex = startIndex + itemsPerPage;
 
     // Filter data
-    let filteredData = activeChain.data;
+    let filteredData = selectedChain.data;
 
     if (filterKeyword) {
-      filteredData = activeChain.data.filter(item => {
+      filteredData = selectedChain.data.filter(item => {
         const hostMatch = getHost(item.url).toLowerCase().includes(filterKeyword.toLowerCase());
         return hostMatch;
       });
       console.log('Filtered Data length:', filteredData.length);
     }
-
+  
     // Calculate up and downvotes, totalVotes, and trend
     const urlVotesMap = new Map();
     let totalUpvotes = 0;
@@ -280,10 +274,12 @@ app.get('/aggregate', async (req, res) => {
     console.log('Paginated data length:', paginatedData.length);
 
     res.json(paginatedData);
+}
   } catch (error) {
     console.error('Error processing aggregate request:', error);
     res.status(500).send('Internal Server Error');
   }
+  
 });
 
 // Refreshing stuff
@@ -321,10 +317,10 @@ function getHost(url) {
     return match && match[1];
 }
 
-async function loadData() {
+async function loadData(chain) {
   try {
-    const newData = await getVoteEvents();
-    activeChain.data = [...activeChain.data, ...newData];
+    const newData = await getVoteEvents(chain);
+    chain.data = [...chain.data, ...newData];
     console.log('Data loaded.');
     console.log('Refresh speed:' + refreshInterval);
   } catch (error) {
@@ -332,65 +328,69 @@ async function loadData() {
   }
 }
 
-async function getVoteEvents() {
+async function getVoteEvents(chain) {
     let events = [];
-
     try {
-        if (activeChain.lastBlockNumber === undefined) {
-            // Initial fetch: get events from the latest 500 blocks
-            activeChain.latestBlockNumber = await _provider.getBlockNumber();
-            activeChain.fromBlock = Math.max(0, activeChain.latestBlockNumber - 1000);
-            activeChain.toBlock = 'latest';
+        if (chain.lastBlockNumber === undefined) {
+            chain.latestBlockNumber = await chain.provider.getBlockNumber();
+            chain.toBlock = 'latest';
 
-            const eventFilter = _directNetworkContract.filters.Voted();
-            eventFilter.fromBlock = activeChain.fromBlock;
-            eventFilter.toBlock = activeChain.toBlock;
+            const eventFilter = chain.contract.filters.Voted();
+            eventFilter.fromBlock = chain.fromBlock;
+            eventFilter.toBlock = chain.toBlock;
             //console.log(eventFilter);
-            const logs = await _provider.getLogs(eventFilter);
+            const logs = await chain.provider.getLogs(eventFilter);
             console.log('Loading Initial Data...');
+            console.log('new fromBlock1:', chain.lastBlockNumber);
             for (const log of logs) {
              try {
-              //console.log(log.data);
-              const data = _interface.decodeEventLog("Voted", log.data);
-              events.push({url: data.url, upvote: data.up, title: data.title});
-             } catch (error) {
+              const data = chain.interface.decodeEventLog("Voted", log.data);
+
+              if (data.title !== '') {
+                events.push({url: data.url, upvote: data.up, title: data.title});
+            } else {
+                console.warn('Empty title found in decoded data:', data);
+            }
+            } catch (error) {
               console.error('Error fetching at initial Load', error);
              }
             }
 
-            activeChain.lastBlockNumber = activeChain.latestBlockNumber;
+            chain.lastBlockNumber = chain.latestBlockNumber;
         } else {
             // Subsequent fetch: get new events from the latest block onwards
-            activeChain.latestBlockNumber = await _provider.getBlockNumber();
-            if (activeChain.latestBlockNumber === activeChain.lastBlockNumber) {
+            chain.latestBlockNumber = await chain.provider.getBlockNumber();
+            if (chain.latestBlockNumber === chain.lastBlockNumber) {
                 return events;
             }
-            activeChain.fromBlock = activeChain.lastBlockNumber + 1;
-            activeChain.toBlock = activeChain.latestBlockNumber;
+            chain.fromBlock = chain.lastBlockNumber + 1;
+            chain.toBlock = chain.latestBlockNumber;
 
-            const eventFilter = _directNetworkContract.filters.Voted();
-            eventFilter.fromBlock = activeChain.fromBlock;
-            eventFilter.toBlock = activeChain.toBlock;
+            const eventFilter = chain.contract.filters.Voted();
+            eventFilter.fromBlock = chain.fromBlock;
+            eventFilter.toBlock = chain.toBlock;
             //console.log(eventFilter);
 
 
-            const logs = await _provider.getLogs(eventFilter);
+            const logs = await chain.provider.getLogs(eventFilter);
             console.log('Loading New Data...');
+            console.log('new fromBlock2:', chain.lastBlockNumber);
+
             for (const log of logs) {
               try {
                 //console.log(log.data);
 
-                const data = _interface.decodeEventLog("Voted", log.data);
+                const data = chain.interface.decodeEventLog("Voted", log.data);
                 events.push({url: data.url, upvote: data.up, title: data.title});
               } catch (error) {
                 console.error('Error reinitial load' + error);
               }
             }  
-            activeChain.lastBlockNumber = activeChain.latestBlockNumber;
+            chain.lastBlockNumber = chain.latestBlockNumber;
         }
     } catch (error) {
         console.error('Error fetching vote events:', error);
     }
-
+    console.log(chain.lastBlockNumber);
     return events;
 }
