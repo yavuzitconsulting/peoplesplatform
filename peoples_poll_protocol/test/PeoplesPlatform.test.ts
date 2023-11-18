38c71c06f6a6e087f5068fe48bfce0ecebe582c3
cg <cg@cryptng.com>
2023-11-18 09:36:03 +0100

    add simple donate and vote test

--- peoples_poll_protocol/test/PeoplesPlatform.test.ts
+++ peoples_poll_protocol/test/PeoplesPlatform.test.ts
@@ -12,4 +12,24 @@ describe("PeoplesPlatform contract", function() {
 
     expect(balance).to.equal(0n);
   });
+
+  it("should be able to donate some amount for this year", async function() {
+    await deployments.fixture(["PeoplesPlatform"]);
+    const {deployer} = await getNamedAccounts();
+    const PeoplesPlatform = await ethers.getContract("PeoplesPlatform");
+    const donate120Finney = parseUnits('120',15);
+    await PeoplesPlatform.donate(12,12,2022,{value:donate120Finney});
+    const balance = await ethers.provider.getBalance(PeoplesPlatform.getAddress());
+    
+    expect(balance).to.equal(donate120Finney);
+  });
+
+  it("should be able to vote on some content", async function() {
+    await deployments.fixture(["PeoplesPlatform"]);
+    const {deployer} = await getNamedAccounts();
+    const PeoplesPlatform = await ethers.getContract("PeoplesPlatform");
+    await PeoplesPlatform.vote(true,deployer,'https://www.construction-physics.com/p/how-the-gas-turbine-conquered-the', 1,2023);
+  
+    
+  });
 });
