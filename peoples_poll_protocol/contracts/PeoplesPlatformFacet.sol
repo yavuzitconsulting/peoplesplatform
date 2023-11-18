3ea0fdc274341465aeb9752c06a17f4bae319505
cg <cg@cryptng.com>
2023-11-18 18:46:35 +0100

    UX extenstion

--- peoples_poll_protocol/contracts/PeoplesPlatformFacet.sol
+++ peoples_poll_protocol/contracts/PeoplesPlatformFacet.sol
@@ -1,6 +1,6 @@
 //SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.10;
 import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 //import "hardhat/console.sol";
