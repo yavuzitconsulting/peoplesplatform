5957fcce25c007e52ea6add0341afe9533c10b71
Ramazan Yavuz <info@yavuz.support>
2023-11-18 14:05:08 +0100

    add youtube / todo reddit, styling, actual behavior, SC interactions

--- peoples_choice_extender/youtube.js
+++ peoples_choice_extender/youtube.js
@@ -6,14 +6,23 @@ function injectCustomDiv() {
         if (targetDiv) {
           const customDiv = document.createElement('div');
           customDiv.className = 'custom-mine';
-          customDiv.style.background = 'white';
-          customDiv.innerHTML = '<button id="myButton">Click me</button>';
-      
+            customDiv.style.display="flex";
+            customDiv.style.flexDirection="row";
+            customDiv.style.marginRight="2rem";
+            
+          const buttonId = generateUniqueAlphanumericId();
+          const buttonIdLike = buttonId+"L";  
+          const buttonIdDislike = buttonId+"D";  
+          customDiv.innerHTML = `<button id="${buttonIdLike}" style="background: none; cursor: pointer; outline: none; padding: 0; margin: 0; width: 3rem; user-select: none; border: white solid 1px; height: 3rem; border-radius: 20%;">&#x1F44D;</button> <button id="${buttonIdDislike}" style="background: none; cursor: pointer; outline: none; padding: 0; margin: 0; width: 3rem; user-select: none; border: white solid 1px; height: 3rem; border-radius: 20%;">&#x1F44E;</button>`;
+
+
           targetDiv.prepend(customDiv);
       
-          document.getElementById('myButton').addEventListener('click', () => {
-            //here we want to check if the wallet is connected etc, we do that with the injectorScript
-            sendUpvoteMessageToInjectedScript(window.location.href)
+          document.getElementById(buttonIdLike).addEventListener('click', () => {
+            sendUpvoteMessageToInjectedScript(window.location.href);
+            });
+          document.getElementById(buttonIdDislike).addEventListener('click', () => {
+              sendDownvoteMessageToInjectedScript(window.location.href);
           });
         }
 
@@ -22,7 +31,18 @@ function injectCustomDiv() {
   }
 
 
-  
+
+  function generateUniqueAlphanumericId() {
+    const alphanumericCharacters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let id = '';
+    for (let i = 0; i < 10; i++) { // You can adjust the length of the ID as needed
+      const randomIndex = Math.floor(Math.random() * alphanumericCharacters.length);
+      id += alphanumericCharacters.charAt(randomIndex);
+    }
+    return id;
+  }
+
+  // This is your content script
 function injectScript(file, node) {
     const th = document.getElementsByTagName(node)[0];
     const s = document.createElement('script');
@@ -32,20 +52,25 @@ function injectScript(file, node) {
 }
 
 
-
+// Example of sending a message to the injected script
 function sendMessageToInjectedScript(message) {
     window.postMessage({ type: "FROM_CONTENT", customMessage: message }, "*");
 }
 
 function sendUpvoteMessageToInjectedScript(url) {
-    window.postMessage({ type: "UPVOTE_CONTENT", contentUrl: url }, "*");
+    window.postMessage({ type: "PCE_UPVOTE_CONTENT", contentUrl: url }, "*");
 }
 
-
+// Listen for messages from the injected script
 window.addEventListener('message', function(event) {
-    
+    // Ensure the message is from your injected script
     if (event.source === window && event.data.type && event.data.type == "FROM_PAGE") {
         console.log("Message received from injected script:", event.data);
+    }
+    
+    if (event.source === window && event.data.type && event.data.type == "PCE_SITE_NAVIGATED") {
+        console.log("Page was navigated");
+        injectCustomDiv();
     }
 });
 
@@ -54,12 +79,9 @@ window.addEventListener('message', function(event) {
 window.onload = () => {
     injectCustomDiv();
 
-    // Inject ethers.js library first
-    injectScript(chrome.runtime.getURL('ethers.min-6.8.1.js'), 'body'); 
+    // Then inject your custom script
+    injectScript(chrome.runtime.getURL('contentPageInjectedScript.js'), 'body');
 
-    
-    injectScript(chrome.runtime.getURL('injectedscript.js'), 'body');
-
-    
+    // Example usage
     sendMessageToInjectedScript("Hello from content script!");
 };
